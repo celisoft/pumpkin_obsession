137308c535c98265f904359d11d23b088a0fe36a
Celine Liberal <celine.liberal@mail.com>
2014-11-13 12:28:04 +0100

    Distutils for source distribution (sdist)

---
+++ setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+	name='Pumpkin Obsession',
+	version='1.0',
+	license='BSD',
+	description='Python arcade game with a skeleton and some pumpkins',
+	author='Celine Liberal, Zoe Belleton',
+	author_email='celine.liberal@mail.com',
+	url='http://www.celisoft.com/games/pumpkin_obsession/',
+	packages=['src', 'src.gameobjects']
+)
