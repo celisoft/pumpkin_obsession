9a9eb384491ba4d098ec7fb345d42887ea7ed54a
Celine Liberal <celine.liberal@mail.com>
2014-11-16 15:43:49 +0100

    Start code cleanup + key repeat

--- src/main.py
+++ src/main.py
@@ -43,27 +43,27 @@ class PumpkinObsession():
 		pygame.mixer.music.play(-1)
 
 		#Create game sounds object
-		get_pumpkin_sound = pygame.mixer.Sound(self.SND_GET_PUMPKIN)
-		loose_game_sound = pygame.mixer.Sound(self.SND_LOOSE_GAME)
+		self.snd_get_pumpkin = pygame.mixer.Sound(self.SND_GET_PUMPKIN)
+		self.snd_loose_game = pygame.mixer.Sound(self.SND_LOOSE_GAME)
 
 		#Initalize the main game menu
-		lMenuScene = scene.MenuScene("IMG_GAME_INIT")
-		lMenuScene.add_menu_entry(scene.MenuSceneEntry("Play"))
-		lMenuScene.add_menu_entry(scene.MenuSceneEntry("Quit"))
+		self.menu_scene = scene.MenuScene("IMG_GAME_INIT")
+		self.menu_scene.add_menu_entry(scene.MenuSceneEntry("Play"))
+		self.menu_scene.add_menu_entry(scene.MenuSceneEntry("Quit"))
 
 		#Initialize the game scene with player and pumpkin manager
-		lGameScene = scene.GameScene()
-		lPlayer = player.Player(lGameScene)
-		lPumpkins = pumpkin.PumpkinManager(lGameScene.screen)
-		lGround = ground.Ground(lGameScene.screen)
+		self.game_scene = scene.GameScene()
+		self.player = player.Player(self.game_scene)
+		self.pumpkin_manager = pumpkin.PumpkinManager(self.game_scene.screen)
+		self.ground = ground.Ground(self.game_scene.screen)
 
-		#The game is not yet started and is not ended
-		gameStarted = False
-		gameEnded = False
-		gamePaused = False
+		#The game is not yet started, paused and not ended
+		self.game_started = False
+		self.game_ended = False
+		self.game_paused = False
 
 		#Allow key repeat
-		#pygame.key.set_repeat(50, 50)
+		pygame.key.set_repeat(50, 50)
 		
 		#Add a USEREVENT that will be used to generate a pumpkin every 2 seconds 
 		pygame.time.set_timer(pygame.USEREVENT, 2000)
@@ -71,84 +71,49 @@ class PumpkinObsession():
 		#Add a USEREVENT that will reset notifaction area every 3.5 seconds 
 		pygame.time.set_timer(pygame.USEREVENT+1, 3500)
 
-		lClock = pygame.time.Clock()
+		self.clock = pygame.time.Clock()
 
 		#Cycle while the game is not finished
-		while not gameEnded:
-			if gameStarted:	
+		while not self.game_ended:
+			if self.game_started:	
 				#Game loop
-			
-				if gamePaused:
-					lGameScene.clear_screen()
-					lGameScene.update_notification_area("Game paused")
-					lGameScene.refresh()
+				self.game_scene.clear_screen()
+				self.game_scene.refresh()
+
+				if self.game_paused:
+					self.game_scene.update_notification_area("Game paused")
 
 					for event in pygame.event.get():
 						if event.type == pygame.KEYDOWN:
-							gamePaused = False
-
-					lPlayer.draw()
-					lGround.draw()
+							self.game_paused = False
 				else:
-					lGameScene.clear_screen()
-					lGameScene.refresh()
-			
 					#Set the player waiting position
-					lPlayer.wait()
-			
-					for event in pygame.event.get():
-						if event.type == pygame.USEREVENT:
-							#Create a new pumpkin
-							lPumpkins.generate_pumpkin()						
-						elif event.type == pygame.KEYDOWN:
-							if event.key == pygame.K_RIGHT:
-								lPlayer.move_right()
-							elif event.key == pygame.K_LEFT:
-								lPlayer.move_left()
-							elif event.key == pygame.K_SPACE or event.key == pygame.K_UP:
-								lPlayer.jump()
-							elif event.key == pygame.K_f:
-								lGameScene.toggle_fullscreen()
-							elif event.key == pygame.K_ESCAPE or event.key == pygame.K_h:
-								gamePaused = True
-							elif event.key == pygame.K_q:
-								gameEnded = True
-						elif event.type == pygame.USEREVENT+1:
-							#Erase the notification
-							lGameScene.reset_notification_area()
-						elif event.type == pygame.QUIT:
-							gameEnded = True
+					self.player.wait()
+					self.check_game_event()
 
 					#Check if the user catch something
-					if lPumpkins.collide_with_player(lPlayer):
-						lPlayer.blink()
-						get_pumpkin_sound.play()
-						levelup = lPlayer.score_update()
+					if self.pumpkin_manager.collide_with_player(self.player):
+						self.player.blink()
+						self.snd_get_pumpkin.play()
+						levelup = self.player.score_update()
 						if levelup:
 							#Increase the game speed
-							lPumpkins.increase_pumpkin_speed(lPlayer.level)
-					elif lPumpkins.collide_with_ground(lGround):
-						lPlayer.loose_live()
-						if lPlayer.get_lives() == 0:
-							#Reset player
-							lPlayer.reset_data()
-						
-							#Reset game scene
-							lGameScene.reset()
+							self.pumpkin_manager.increase_pumpkin_speed(self.player.level)
+					elif self.pumpkin_manager.collide_with_ground(self.ground):
+						self.player.loose_live()
+						if self.player.get_lives() == 0:
+							self.reset()
 							
-							#Reset pumpkin manager
-							lPumpkins.reset()
-
 							#Stop the game background music
 							pygame.mixer.music.stop()
 
 							#Play loose game sound
-							loose_game_sound.play()
+							self.snd_loose_game.play()
 							
 							#Display the loose game scene if the player loose the game
-							lEndScene = scene.TransitionScene("IMG_END")
-							lEndScene.clear_screen()
-							lEndScene.display()
+							self.end_scene = scene.TransitionScene("IMG_END")
+							self.end_scene.clear_screen()
+							self.end_scene.display()
 							pygame.display.flip()
 							pygame.time.wait(4500)
 
@@ -156,42 +121,79 @@ class PumpkinObsession():
 							pygame.mixer.music.play(-1)
 
 							#Back to start menu
-							gameStarted = False
-
-					#Display the ground
-					lGround.draw()
-			
-					#Display the player
-					lPlayer.draw()
-	
+							self.game_started = False
+					
 					#Display all pumpkins
-					lPumpkins.move_and_draw()
+					self.pumpkin_manager.move_and_draw()
+				
+				#Display the ground
+				self.ground.draw()
+			
+				#Display the player
+				self.player.draw()
 			else:
 				#Menu loop
-				lMenuScene.clear_screen()
-				lMenuScene.display()
-
-				for event in pygame.event.get():
-					if event.type == pygame.KEYDOWN:
-						if event.key == pygame.K_RIGHT:
-							lMenuScene.update_cursor_position()
-						elif event.key == pygame.K_LEFT:
-							lMenuScene.update_cursor_position()
-						elif event.key == pygame.K_SPACE or event.key == pygame.K_RETURN:
-							if lMenuScene.get_selection() == 0:
-								gameStarted = True
-							else:
-								gameEnded = True
-					elif event.type == pygame.QUIT:
-						gameEnded = True
+				self.menu_scene.clear_screen()
+				self.menu_scene.display()
+				self.check_menu_event()
 
 			#We only need 60 FPS not more (avoid screen blink)
 			pygame.time.wait(60)
-			lClock.tick(60)
+			self.clock.tick(60)
 			pygame.display.flip()
 
 		#Cleanly exit pygame
 		pygame.quit()
+
+	def check_menu_event(self):
+		""" Check menu events and do something adapted to """
+		for event in pygame.event.get():
+			if event.type == pygame.KEYDOWN:
+				if event.key == pygame.K_RIGHT or event.key == pygame.K_LEFT:
+					self.menu_scene.update_cursor_position()
+				elif event.key == pygame.K_SPACE or event.key == pygame.K_RETURN:
+					if self.menu_scene.get_selection() == 0:
+						self.game_started = True
+					else:
+						self.game_ended = True
+			elif event.type == pygame.QUIT:
+				self.game_ended = True
+
+	def check_game_event(self):
+		""" Check game events and do something adapted to """
+		for event in pygame.event.get():
+			if event.type == pygame.USEREVENT:
+				#Create a new pumpkin
+				self.pumpkin_manager.generate_pumpkin()					
+			elif event.type == pygame.KEYDOWN:
+				if event.key == pygame.K_RIGHT:
+					self.player.move_right()
+				elif event.key == pygame.K_LEFT:
+					self.player.move_left()
+				elif event.key == pygame.K_SPACE or event.key == pygame.K_UP:
+					self.player.jump()
+				elif event.key == pygame.K_f:
+					self.game_scene.toggle_fullscreen()
+				elif event.key == pygame.K_ESCAPE or event.key == pygame.K_h:
+					self.game_paused = True
+				elif event.key == pygame.K_q:
+					self.game_ended = True
+			elif event.type == pygame.USEREVENT+1:
+				#Erase the notification
+				self.game_scene.reset_notification_area()
+			elif event.type == pygame.QUIT:
+				self.game_ended = True
+
+	def reset(self):
+		""" Reset game data to replay """
+		#Reset player
+		self.player.reset_data()
+						
+		#Reset game scene
+		self.game_scene.reset()
+							
+		#Reset pumpkin manager
+		self.pumpkin_manager.reset()
 
 if __name__ == "__main__":
 	PumpkinObsession()
